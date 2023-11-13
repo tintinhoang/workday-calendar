338a789289dcdad558475fbb486c453259de0885
Tintin Hoang <>
2023-11-13 22:22:42 +0100

    feat: create interface for workday calendar

--- src/main/java/no/vivende/WorkdayCalendar.java
+++ src/main/java/no/vivende/WorkdayCalendar.java
@@ -1,4 +1,36 @@
 package no.vivende;
 
-public class WorkdayCalendar {
+import java.util.Calendar;
+import java.util.Date;
+
+interface IWorkdayCalendar {
+    void setHoliday(Calendar date);
+
+    void setRecurringHoliday(Calendar date);
+
+    void setWorkdayStartAndStop(Calendar start, Calendar stop);
+
+    Date getWorkdayIncrement(Date startDate, float incrementInWorkdays);
+}
+
+public class WorkdayCalendar implements IWorkdayCalendar {
+    @Override
+    public void setHoliday(Calendar date) {
+
+    }
+
+    @Override
+    public void setRecurringHoliday(Calendar date) {
+
+    }
+
+    @Override
+    public void setWorkdayStartAndStop(Calendar start, Calendar stop) {
+
+    }
+
+    @Override
+    public Date getWorkdayIncrement(Date startDate, float incrementInWorkdays) {
+        return null;
+    }
 }
